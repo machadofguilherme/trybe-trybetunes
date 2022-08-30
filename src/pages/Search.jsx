242f095abd2fed4fdac0aa6f73b133428adffd16
Guilherme Machado <machadofguilherme@gmail.com>
2022-08-30 19:46:33 -0300

    Requisito 5.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    isDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+
+    if (value >= 2) {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
+  };
+
   render() {
+    const { isDisabled } = this.state;
+
     return (
       <main>
         <Header />
         <div data-testid="page-search">
-          Busca.
+          <form>
+            <input
+              type="text"
+              placeholder="Nome do artista"
+              onChange={ this.handleChange }
+              data-testid="search-artist-input"
+            />
+
+            <button
+              type="submit"
+              disabled={ isDisabled }
+              data-testid="search-artist-button"
+            >
+              Pesquisar
+            </button>
+          </form>
         </div>
       </main>
     );
